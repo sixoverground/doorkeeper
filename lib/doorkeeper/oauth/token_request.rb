f8e73c11be9c55adfaab30d2208df58f8bc87d7a
Craig Phares <craigphares@sixoverground.com>
2018-04-12 12:42:25 -0400

    more logs

--- lib/doorkeeper/oauth/token_request.rb
+++ lib/doorkeeper/oauth/token_request.rb
@@ -4,23 +4,28 @@ module Doorkeeper
       attr_accessor :pre_auth, :resource_owner
 
       def initialize(pre_auth, resource_owner)
+        puts "init token request"
         @pre_auth       = pre_auth
         @resource_owner = resource_owner
       end
 
       def authorize
+        puts "authorize token"
         if pre_auth.authorizable?
+          puts "pre auth authorizable"
           auth = Authorization::Token.new(pre_auth, resource_owner)
           auth.issue_token
           @response = CodeResponse.new pre_auth,
                                        auth,
                                        response_on_fragment: true
         else
+          puts "not authorizable"
           @response = error_response
         end
       end
 
       def deny
+        puts "deny token"
         pre_auth.error = :access_denied
         error_response
       end
